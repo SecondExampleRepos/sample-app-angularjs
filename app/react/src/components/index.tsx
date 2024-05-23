c8f71d6864944448394c3442b5554bffc8dd8754
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-23 16:00:09 +0000

    create_file_with_ai

---
+++ app/react/src/components/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexComponent: React.FC = () => {
+  return (
+    <div>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root"></div>
+    </div>
+  );
+};
+
+export default IndexComponent;
