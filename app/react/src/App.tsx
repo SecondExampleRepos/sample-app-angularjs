8e0df54bb0e768d01431776b07d42feceab44d4e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-27 10:21:03 +0000

    create_file_with_ai

--- app/react/src/App.tsx
+++ app/react/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import useRootScope from './hooks/useRootScope';
 
-function App() {
+// Importing all the components
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+import Home from './components/Home';
+import Contacts from './components/Contacts';
+import Prefs from './components/Prefs';
+import MyMessages from './components/MyMessages';
+
+const AppRouter: React.FC = () => {
+  const { someState, setSomeState, someFunction } = useRootScope();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/prefs" component={Prefs} />
+        <Route path="/mymessages" component={MyMessages} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
-export default App;
+export default AppRouter;
