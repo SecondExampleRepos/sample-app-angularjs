03cb095527732eb509650cfb263782ba1d6291ba
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-12 16:25:41 +0000

    create_file_with_ai

---
+++ react/src/components/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="description" content="Web site created using create-react-app" />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <title>React App</title>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root"></div>
+    </div>
+  );
+};
+
+export default Index;
