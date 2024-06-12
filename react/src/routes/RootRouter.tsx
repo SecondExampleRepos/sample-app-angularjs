545d85efd86e77f1878e7df986db026220689111
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-12 16:25:10 +0000

    create_file_with_ai

---
+++ react/src/routes/RootRouter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+const RootRouter: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+
+        <Route path="/services" component={Services} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default RootRouter;
