2e98e69615e8fb990f6634ee2194efbf3612c6a9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-12 16:24:58 +0000

    create_file_with_ai

---
+++ react/src/services/LoadingIndicatorService.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+export class LoadingIndicatorService {
+  private body: HTMLElement;
+
+  constructor() {
+    this.body = document.body;
+  }
+
+  showLoadingIndicator() {
+    const spinner = document.createElement('div');
+    spinner.id = 'spinner';
+    spinner.innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw" aria-hidden="true"></i>';
+    this.body.appendChild(spinner);
+  }
+
+  hideLoadingIndicator() {
+    const spinner = document.getElementById('spinner');
+    if (spinner && spinner.parentElement) {
+      spinner.parentElement.removeChild(spinner);
+    }
+  }
+}
